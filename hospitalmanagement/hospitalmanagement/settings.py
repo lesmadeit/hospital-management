dae584f00dee79ad3d20780a60cc6f33d2c07393
lesmadeit <lesmwendwa@gmail.com>
2025-08-07 15:48:51 +0300

    changes

--- hospitalmanagement/hospitalmanagement/settings.py
+++ hospitalmanagement/hospitalmanagement/settings.py
@@ -129,9 +129,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "hospital/static")
-]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+#STATICFILES_DIRS = [
+ #   os.path.join(BASE_DIR, "hospital/static")
+#]
+
+
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
